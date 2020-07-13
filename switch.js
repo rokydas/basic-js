87f0e8a78a6aefe961aa1d53d0cbd0c2f476b468
Roky Das <droky136@gmail.com>
2020-07-13 13:35:20 +0600

    switch added

---
+++ switch.js
@@ -0,0 +1,17 @@
+num = 10000;
+switch(num){
+    case 1000:
+        console.log("I am 1000");
+        break;
+    case 500:
+        console.log("I am 500");
+        break;
+    case 100:
+        console.log("I am 100");
+        break;
+    case 50:
+        console.log("I am 50");
+        break;
+    default:
+        console.log("I don't know who I am");
+}
